1c6324054aa32c4459aba7bbda275833f5812eca
Tharma <karthik@vmgdigital.com>
2023-09-12 17:03:51 +0530

    reduced Size'

--- src/assets/js/app1.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	"use strict";
-	$(".mobile-toggle-menu").on("click", function () {
-		$(".wrapper").addClass("toggled")
-	});
-	$(".toggle-icon").click(function () {
-		$(".wrapper").hasClass("toggled") ? $(".wrapper").removeClass("toggled") : $(".wrapper").addClass("toggled");
-	});
-});
